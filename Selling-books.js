72831d1f3f7aa60a6bf0b1e94df0f8445bba380c
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-12-18 21:21:42 -0800

    Update Selling-books.js

--- Selling-books.js
+++ Selling-books.js
@@ -36,3 +36,17 @@ Example:
 
 
 // Solution
+
+function stockList(listOfArt, listOfCat) {
+  let qs = {};
+  if (!listOfArt.length) return '';
+
+  listOfArt.forEach(function(art) {
+    var cat = art[0];
+    qs[cat] = (qs[cat] | 0) + +art.split(' ')[1];
+  });
+
+  return listOfCat.map(function(c) {
+    return '(' + c + ' : ' + (qs[c] | 0) + ')';  
+  }).join(' - ');  
+}
